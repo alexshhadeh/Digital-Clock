553392e243b41a6b74d1f8b7a1ef615770c90a40
alexshhadeh <alexsh2001@hotmail.com>
2022-10-09 15:40:23 +0200

    bugfix

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ const clock = new Clock();
 
 //Set time to Thu Mar 22 2012 09:11:22 GMT+0100 (Central European Standard Time)
 //UNCOMMENT LINE BELOW TO SET THE TIME
-clock.setTime(1332403882588);
+//clock.setTime(1332403882588);
 
 setInterval(() => {
     clock.update();
